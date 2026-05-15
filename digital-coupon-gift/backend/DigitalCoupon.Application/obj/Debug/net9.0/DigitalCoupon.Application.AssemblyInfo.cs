5a68d20f6b26a87286657143502362b464aad31f
Juliano Galhardo <jubagalhardo9@gmail.com>
2026-05-15 15:31:07 -0300

    feat: force EF Core v9.0.0 and setup dependency injection layers

--- digital-coupon-gift/backend/DigitalCoupon.Application/obj/Debug/net9.0/DigitalCoupon.Application.AssemblyInfo.cs
+++ digital-coupon-gift/backend/DigitalCoupon.Application/obj/Debug/net9.0/DigitalCoupon.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DigitalCoupon.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fe093f44b04f7f1b865f69b8b45967737d3341d4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6f807d803a4e11ff7cc7f34e00f7dbbb15e1990d")]
 [assembly: System.Reflection.AssemblyProductAttribute("DigitalCoupon.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DigitalCoupon.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
