6dc988b0f0db41d153ae4b7b2b8f8152b56cb02d
Juliano Galhardo <jubagalhardo9@gmail.com>
2026-05-15 15:47:48 -0300

    test

--- digital-coupon-gift/backend/DigitalCoupon.Domain/Common/Entity.cs
+++
@@ -1,7 +0,0 @@
-namespace DigitalCoupon.Domain.Common;
-
-public abstract class Entity
-{
-    public Guid Id { get; protected set; } = Guid.NewGuid();
-    public DateTime CreatedAt { get; init; } = DateTime.UtcNow;
-}
